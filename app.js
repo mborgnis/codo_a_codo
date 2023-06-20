e6d92c3b9e613c14dfd311472fc689a310f0acbf
mborgnis <micuborg@gmail.com>
2023-06-19 22:15:54 -0300

    Desarrollo y modificaciones ft creacion de server

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3002;
+
+app.use(express.static('public'));
+
+app.listen(port, () => console.log(`Puerto corriendo en ${port}`));
